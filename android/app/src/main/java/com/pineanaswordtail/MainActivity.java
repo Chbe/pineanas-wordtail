4f3a61afb79b96de60211c4c0d4f59ab461855ff
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2020-02-29 16:47:32 +0100

    com.pineanaswordtail => com.pineanas.wordtail

--- android/app/src/main/java/com/pineanaswordtail/MainActivity.java
+++ android/app/src/main/java/com/pineanaswordtail/MainActivity.java
@@ -1,4 +1,4 @@
-package com.pineanaswordtail;
+package com.pineanas.wordtail;
 
 import com.facebook.react.ReactActivity;
 
--- android/app/src/main/java/com/pineanaswordtail/MainApplication.java
+++ android/app/src/main/java/com/pineanaswordtail/MainApplication.java
@@ -1,4 +1,4 @@
-package com.pineanaswordtail;
+package com.pineanas.wordtail;
 
 import android.app.Application;
 import android.content.Context;
