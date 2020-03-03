6964dae579cbd5fad0e52363f46af89f1f3824b6
Christopher Bengtsson <bengtsson.christopher@hotmail.com>
2020-03-03 15:47:04 +0100

    profile page

--- src/pages/profile/ProfilePage.js
+++ src/pages/profile/ProfilePage.js
@@ -1,16 +1,62 @@
-import React from "react";
-import { withTheme, Button } from "react-native-elements";
+import React, { useState, useEffect } from "react";
+import { withTheme, Button, Avatar } from "react-native-elements";
 import { logout } from "../../components/auth/AuthFunctions";
+import firebase from "@react-native-firebase/app";
 import {
   SafeWrapper,
-  PaddingView
+  PaddingView,
+  CenterView
 } from "../../components/ui/containers/Containers";
+import { View } from "react-native";
+import TextField from "../../components/ui/controls/inputs/floating/FloatingInput";
 
 const ProfilePage = ({ theme }) => {
+  const [user, setUser] = useState({});
+  const [displayNameInputValue, setDisplayNameInputValue] = useState('');
+  const [emailInputValue, setEmailInputValue] = useState('');
+
+  useEffect(() => {
+    const { currentUser } = firebase.auth();
+    setUser(currentUser);
+    return () => {
+      //
+    };
+  }, []);
+
   return (
     <SafeWrapper bg={theme.colors.lightShade}>
       {/* <GenerateExampleGames /> */}
-      <PaddingView style={{ height: "100%" }}>
+      <PaddingView>
+        <CenterView>
+          <Avatar
+            size="large"
+            rounded
+            title={user.displayName && user.displayName[0]}
+            source={user.photoURL ? { uri: user.photoURL } : null}
+            showEditButton
+          />
+        </CenterView>
+        <PaddingView
+          style={{
+            margin: 10,
+            borderRadius: 10
+          }}
+        >
+          <TextField
+            baseColor={theme.colors.lightAccent}
+            tintColor={theme.colors.lightAccent}
+            label={user.displayName ? user.displayName : "Anonymous Name"}
+            value={displayNameInputValue}
+            onChangeText={name => setDisplayNameInputValue(name)}
+          />
+          <TextField
+            baseColor={theme.colors.lightAccent}
+            tintColor={theme.colors.lightAccent}
+            label={user.email ? user.email : "Anonymous Email"}
+            value={emailInputValue}
+            onChangeText={email => setEmailInputValue(email)}
+          />
+        </PaddingView>
         <Button title="Signout" onPress={() => logout()} />
       </PaddingView>
     </SafeWrapper>
